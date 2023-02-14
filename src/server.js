2704fd033e866c319e4dd1aae6abf760d7c55830
ryugibo <7928632+ryugibo@users.noreply.github.com>
2023-02-14 12:42:24 +0900

    :memo: #3.3 Responses

--- src/server.js
+++ src/server.js
@@ -2,6 +2,16 @@ import express from "express";
 
 const PORT = 4000;
 const app = express();
+
+const handleHome = (req, res) => {
+  return res.send("I still love you.");
+};
+const handleLogin = (req, res) => {
+  return res.send("login here.");
+};
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+
 const handleListening = () => {
   console.log(`Server listening http://localhost:${PORT}`);
 };
