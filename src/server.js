1f9fe311516106665625c5b1e4a96312c6d12b7e
ryugibo <7928632+ryugibo@users.noreply.github.com>
2023-02-15 01:01:29 +0900

    :memo: #3.6 Middlewares part Two

--- src/server.js
+++ src/server.js
@@ -3,18 +3,17 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
-const gossipmiddleware = (req, res, next) => {
-  console.log("I'm in the middle!");
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
   next();
 };
+
 const handleHome = (req, res) => {
   return res.send("I still love you.");
 };
-const handleLogin = (req, res) => {
-  return res.send("login here.");
-};
-app.get("/", gossipmiddleware, handleHome);
-app.get("/login", handleLogin);
+
+app.use(logger);
+app.get("/", handleHome);
 
 const handleListening = () => {
   console.log(`Server listening http://localhost:${PORT}`);
