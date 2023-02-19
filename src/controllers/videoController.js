85f1da85826ba1a9702686de50d9bb675c830c95
ryugibo <7928632+ryugibo@users.noreply.github.com>
2023-02-19 14:14:47 +0900

    :memo: #4.6 Planning Routes

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,3 +1,6 @@
 export const trending = (req, res) => res.send("Home Page Videos.");
-export const watch = (req, res) => res.send("Watch Video");
+export const see = (req, res) => res.send("Watch Video");
 export const edit = (req, res) => res.send("Edit Video");
+export const search = (req, res) => res.send("Search Videos");
+export const upload = (req, res) => res.send("Upload Videos");
+export const remove = (req, res) => res.send("Remove Videos");
