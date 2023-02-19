94e63f98b1128acef95c94438af97f7167bf5140
ryugibo <7928632+ryugibo@users.noreply.github.com>
2023-02-19 22:29:44 +0900

    :memo: #5.2 Partials

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,6 @@
 export const trending = (req, res) => res.render("home");
-export const see = (req, res) => res.send(`Watch Video #${req.params.id}`);
-export const edit = (req, res) => res.send("Edit Video");
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search Videos");
 export const upload = (req, res) => res.send("Upload Videos");
 export const remove = (req, res) => res.send("Remove Videos");
