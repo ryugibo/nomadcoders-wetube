3a60335c71654b69da2f61e9e50c9a1558626586
ryugibo <7928632+ryugibo@users.noreply.github.com>
2023-02-19 22:47:58 +0900

    :memo: #5.4 Variables to Templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,6 @@
-export const trending = (req, res) => res.render("home");
-export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("edit");
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const search = (req, res) => res.send("Search Videos");
 export const upload = (req, res) => res.send("Upload Videos");
 export const remove = (req, res) => res.send("Remove Videos");
