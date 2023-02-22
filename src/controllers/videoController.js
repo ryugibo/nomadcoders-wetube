4cd91923719d1918c0aba3dd7ba95275fae8a3b8
ryugibo <7928632+ryugibo@users.noreply.github.com>
2023-02-22 21:34:44 +0900

    :memo: #6.2 Edit Video part One

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -31,10 +31,11 @@ export const trending = (req, res) => {
 export const watch = (req, res) => {
   const { id } = req.params;
   const video = videos[id - 1];
-  console.log(video);
-  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+  return res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
 };
-export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
-export const search = (req, res) => res.send("Search Videos");
-export const upload = (req, res) => res.send("Upload Videos");
-export const remove = (req, res) => res.send("Remove Videos");
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+};
+export const postEdit = (req, res) => {};
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,10 @@
 import express from "express";
-import { edit, upload, remove, watch } from "../controllers/videoController";
+import { watch, getEdit, postEdit } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id(\\d+)", watch);
-videoRouter.get("/:id(\\d+)/edit", edit);
-videoRouter.get("/:id(\\d+)/delete", remove);
-videoRouter.get("/upload", upload);
+videoRouter.get("/:id(\\d+)/edit", getEdit);
+videoRouter.post("/:id(\\d+)/edit", postEdit);
 
 export default videoRouter;
