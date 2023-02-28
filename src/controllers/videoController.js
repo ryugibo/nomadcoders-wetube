4a33a867c1c367d0ceb456b7a80b74818cbee361
ryugibo <7928632+ryugibo@users.noreply.github.com>
2023-03-01 02:40:54 +0900

    :memo: #6.16 Creating a Video part Two

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,6 +2,7 @@ import Video from "../models/Video";
 
 export const home = async (req, res) => {
   const videos = await Video.find({});
+  console.log(videos);
   return res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = (req, res) => {
@@ -20,15 +21,14 @@ export const postEdit = (req, res) => {
 export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
   const { title, description, hashtags } = req.body;
-  const video = new Video({
+  await Video.create({
     title,
     description,
     createdAt: Date.now(),
     meta: { views: 0, rating: 0 },
     hashtags: hashtags.split(",").map((word) => `#${word}`),
   });
-  console.log(video);
   return res.redirect("/");
 };
