3737a64a34cc5cdd5a52a49485d1a07718ead3b7
ryugibo <7928632+ryugibo@users.noreply.github.com>
2023-03-03 15:15:21 +0900

    :memo: #6.24 Statics

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -31,7 +31,7 @@ export const postEdit = async (req, res) => {
   await Video.findByIdAndUpdate(id, {
     title,
     description,
-    hashtags,
+    hashtags: Video.formatHashtags(hashtags),
   });
   return res.redirect(`/videos/${id}`);
 };
@@ -44,7 +44,7 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags,
+      hashtags: Video.formatHashtags(hashtags),
     });
     return res.redirect("/");
   } catch (error) {
