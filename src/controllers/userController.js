7eb6733216b5e2539aa46d995792314762e1f5b7
ryugibo <7928632+ryugibo@users.noreply.github.com>
2023-03-03 17:39:03 +0900

    :memo: #7.5-6 Login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => {
   return res.render("join", { pageTitle: "Create Account" });
@@ -21,11 +22,41 @@ export const postJoin = async (req, res) => {
       errorMessage: "This username/email is already taken.",
     });
   }
-  await User.create({ name, email, username, password: password1, location });
-  return res.redirect("/login");
+  try {
+    await User.create({ name, email, username, password: password1, location });
+    return res.redirect("/login");
+  } catch (error) {
+    return res.status(400).render("join", {
+      pageTitle: "Create Account",
+      errorMessage: error._message,
+    });
+  }
+};
+export const getLogin = (req, res) => {
+  return res.render("login", { pageTitle: "Login" });
+};
+export const postLogin = async (req, res) => {
+  const pageTitle = "Login";
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "An account with this username does not exists.",
+    });
+  }
+  console.log(password);
+  console.log(user.password);
+  const ok = await bcrypt.compare(password, user.password);
+  if (!ok) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "Wrong password",
+    });
+  }
+  return res.redirect("/");
 };
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
-export const login = (req, res) => res.send("Login User");
 export const see = (req, res) => res.send("See User");
 export const logout = (req, res) => res.send("Logout User");
