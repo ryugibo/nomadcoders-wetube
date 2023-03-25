f74d9a0ca8ac3d83f7df61463d3a010213dd85f7
ryugibo <7928632+ryugibo@users.noreply.github.com>
2023-03-25 15:41:09 +0900

    :memo: #8.1 Protector and Public Middlewares

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -7,12 +7,24 @@ import {
   postUpload,
   getDelete,
 } from "../controllers/videoController";
+import { protectorMiddleware } from "../middlewares";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id([0-9a-f]{24})", watch);
-videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
-videoRouter.get("/:id([0-9a-f]{24})/delete", getDelete);
-videoRouter.route("/upload").get(getUpload).post(postUpload);
+videoRouter
+  .route("/:id([0-9a-f]{24})/edit")
+  .all(protectorMiddleware)
+  .get(getEdit)
+  .post(postEdit);
+videoRouter
+  .route("/:id([0-9a-f]{24})/delete")
+  .all(protectorMiddleware)
+  .get(getDelete);
+videoRouter
+  .route("/upload")
+  .all(protectorMiddleware)
+  .get(getUpload)
+  .post(postUpload);
 
 export default videoRouter;
