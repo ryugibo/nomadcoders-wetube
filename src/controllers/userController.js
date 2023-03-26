0f3c9ef3c32fc320ae29e41c021ab07fe38a0b9e
ryugibo <7928632+ryugibo@users.noreply.github.com>
2023-03-26 21:25:15 +0900

    :memo: #8.4-5 Change Password

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -160,3 +160,37 @@ export const finishGithubLogin = async (req, res) => {
     return res.redirect("/login");
   }
 };
+
+export const getChangePassword = (req, res) => {
+  if (req.session.user.socialOnly) {
+    return res.redirect("/");
+  }
+  return res.render("users/change-password", { pageTitle: "Change Password" });
+};
+
+export const postChangePassword = async (req, res) => {
+  const {
+    session: {
+      user: { _id: id },
+    },
+    body: { oldPassword, newPassword, newPassword1 },
+  } = req;
+
+  const user = await User.findById(id);
+  const ok = await bcrypt.compare(oldPassword, user.password);
+  if (!ok) {
+    return res.status(400).render("users/change-password", {
+      pageTitle: "Change Password",
+      errorMessage: "The current password is incorrect",
+    });
+  }
+  if (newPassword !== newPassword1) {
+    return res.status(400).render("users/change-password", {
+      pageTitle: "Change Password",
+      errorMessage: "The password does not match the confirmation",
+    });
+  }
+  user.password = newPassword;
+  await user.save();
+  return res.redirect("/logout");
+};
