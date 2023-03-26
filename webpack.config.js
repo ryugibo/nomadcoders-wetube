dff63b62fb7ffa57314651ba76054136c29293a7
ryugibo <7928632+ryugibo@users.noreply.github.com>
2023-03-26 23:33:15 +0900

    :memo: #9.5 MiniCssExtractPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
+  plugins: [new MiniCssExtractPlugin({ filename: "css/styles.css" })],
   entry: "./src/client/js/main.js",
   mode: "development",
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "assets", "js"),
+    filename: "js/main.js",
+    path: path.resolve(__dirname, "assets"),
   },
   module: {
     rules: [
@@ -20,7 +22,7 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
     ],
   },
