6a11fa748bdb27f814ca39b65a02ad7c100deb1e
ryugibo <7928632+ryugibo@users.noreply.github.com>
2023-03-27 00:08:20 +0900

    :memo: #10 Styles

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,6 @@
 import User from "../models/User";
 import bcrypt from "bcrypt";
 import fetch from "node-fetch";
-import Video from "../models/Video";
 
 export const getJoin = (req, res) => {
   return res.render("join", { pageTitle: "Create Account" });
@@ -102,7 +101,13 @@ export const postEdit = async (req, res) => {
 
 export const see = async (req, res) => {
   const { id } = req.params;
-  const user = await User.findById(id).populate("videos");
+  const user = await User.findById(id).populate({
+    path: "videos",
+    populate: {
+      path: "owner",
+      model: "User",
+    },
+  });
   if (!user) {
     return res.status(404).render("404", { pageTitle: "User not found." });
   }
